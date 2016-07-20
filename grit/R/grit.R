9563f34f495c715cd41fc06a41cc6e1cd233587b
Judith Canner <jcanner@csumb.edu>
2016-07-20 10:25:15 -0700

    creation of grit package

---
+++ grit/R/grit.R
@@ -0,0 +1,8 @@
+#' grit
+#'
+#' A package designed to document the analysis of repeatedly measured grit scores on the same subjects in different contexts
+#'
+#'
+#' @docType package
+#' @name grit
+NULL
